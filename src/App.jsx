4e423fed009c79f1f09c8fb5da0a8b2bf1bad5f0
tahaben401 <bentaha2004@gmail.com>
2025-12-11 17:55:10 +0100

    fix: added react router and some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 function App() {
   
 
   return (
     <>
-      
+
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+      </Routes>
     </>
   )
 }
